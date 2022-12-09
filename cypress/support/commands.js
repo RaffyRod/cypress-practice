2177c46f949956bb596088d603f49731ee668777
Edgar Ysabel <edgarysabel@outlook.com>
2022-12-09 00:15:03 -0400

    Adding Login Test cases

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -2,34 +2,14 @@ import LoginPage from "./pageObjects/loginPage";
 
 const loginpage = new LoginPage();
 
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-Cypress.Commands.add('login', ( ) => {
-  cy.visit(Cypress.env('baseUrl'));
-  loginpage.userNameInput().type(Cypress.env('USER_NAME'));
-  loginpage.userPassword().type(Cypress.env('USER_PASSWORD'));
-  loginpage.loginButton().click();
-  loginpage.dashBoardpage().should('have.text', 'Dashboard');
+Cypress.Commands.add("openLogin", () => {
+  cy.visit(Cypress.env("baseUrl"));
 });
 
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add("login", () => {
+  cy.openLogin();
+  loginpage.userNameInput().type(Cypress.env("USER_NAME"));
+  loginpage.userPassword().type(Cypress.env("USER_PASSWORD"));
+  loginpage.loginButton().click();
+  loginpage.dashBoardpage().should("have.text", "Dashboard");
+});
