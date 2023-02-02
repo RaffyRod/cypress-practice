0d492d97bdebe51e14871703a64044edf7938f39
Alfredo Polanco <alfredo.polanco@semsee.com>
2023-02-02 12:33:25 -0400

    Removing comented and unused code

--- cypress/support/inputData.js
+++ cypress/support/inputData.js
@@ -3,23 +3,7 @@ import { faker } from '@faker-js/faker';
 faker.setLocale('en_US');
 
 const userData = {
-  firstName: faker.name.firstName(),
-  replacementName: faker.name.firstName(),
-  lastName: faker.name.lastName(),
   fullName: faker.name.fullName(),
-  jobTitle: faker.name.jobTitle(),
-  password: faker.internet.password(),
-  email: faker.internet.email(),
-  phoneNumberFormat: faker.phone.number('(###) ###-####'),
-  phoneNumberExt: faker.phone.number('###'),
-  address: faker.address.state(),
-  address: faker.address.city(),
-  zipCode: faker.address.zipCode(),
-  zipCodeByState: faker.address.zipCodeByState("AL"),
-  establishedYear: String(Math.floor(Math.random() * (2022 - 1999 + 1 )) + 1999),
-  randomAlphaNumericString: faker.random.numeric(7),
-  editClassCode: faker.random.numeric(7),
-  companyName: faker.company.name()
 };
 
 export const userInfo = {
